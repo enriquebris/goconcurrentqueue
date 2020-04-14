decfc52f0e9449c4ee2670bb70630cba2439b8f0
Enrique Bris <cubanhenry5@gmail.com>
2020-04-14 03:07:18 -0400

    adding DequeueOrWaitForNextElementContext to Queue interface

--- queue.go
+++ queue.go
@@ -1,5 +1,7 @@
 package goconcurrentqueue
 
+import "context"
+
 // Queue interface with basic && common queue functions
 type Queue interface {
 	// Enqueue element
@@ -9,6 +11,10 @@ type Queue interface {
 	// DequeueOrWaitForNextElement dequeues an element (if exist) or waits until the next element gets enqueued and returns it.
 	// Multiple calls to DequeueOrWaitForNextElement() would enqueue multiple "listeners" for future enqueued elements.
 	DequeueOrWaitForNextElement() (interface{}, error)
+	// DequeueOrWaitForNextElementContext dequeues an element (if exist) or waits until the next element gets enqueued and returns it.
+	// Multiple calls to DequeueOrWaitForNextElementContext() would enqueue multiple "listeners" for future enqueued elements.
+	// When the passed context expires this function exits and returns the context' error
+	DequeueOrWaitForNextElementContext(context.Context) (interface{}, error)
 	// Get number of enqueued elements
 	GetLen() int
 	// Get queue's capacity
